7d8de17b71d7976f6851215b92781fee48913de4
IRZYNI <137916351+IRZYNI@users.noreply.github.com>
2024-12-02 09:37:12 +0100

    _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import '../css/main.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9345699029057065"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
